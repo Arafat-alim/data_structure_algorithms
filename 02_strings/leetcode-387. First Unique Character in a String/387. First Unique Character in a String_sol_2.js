c3c20cdb0567828f8cb47705282cda647bb6cc2b
arafat-alim <arafat97alim@gmail.com>
2025-01-17 00:47:16 +0530

    💫 Leetcode: 56. 387. First Unique Character in a String sol 2, solution added.

---
+++ 02_strings/leetcode-387. First Unique Character in a String/387. First Unique Character in a String_sol_2.js
@@ -0,0 +1,15 @@
+function firstUniqueChar(str) {
+  let obj = {};
+
+  for (let char of str) {
+    obj[char] = (obj[char] || 0) + 1;
+  }
+
+  for (let char of str) {
+    if (obj[char] === 1) {
+      return char;
+    }
+  }
+
+  return null;
+}
